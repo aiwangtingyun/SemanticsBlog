601da1d4496c0c9d339c714441ac881600a4c10d
wangtingyun <wangtingyun@aipai.com>
2020-07-09 11:56:51 +0800

    更改为使用外置 tomcat 服务器的 war 包

---
+++ src/main/java/com/lrm/SpringBootStartApplication.java
@@ -0,0 +1,12 @@
+package com.lrm;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class SpringBootStartApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(BlogApplication.class);
+    }
+}
